725876ad1bd35e3584742dc73e0470eac299236f
Ciska <44933905+CiskaLV@users.noreply.github.com>
2023-03-19 23:23:24 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,29 +1,32 @@
 # python3
 import math
-def build_heap(data):
+
+
+def build_heap(n, data):
     swaps = []
 
     def swap(child, parent):
-        if (data[parent] >= data[child]):
-            data[child], data[parent] = data[parent], data[child]
-            swaps.append((parent,child))
+        if (data[parent] <= data[child]):
+            return None
+        data[child], data[parent] = data[parent], data[child]
+        return (child, parent)
 
-    upsideDown = reversed(data)
-    for i in upsideDown:
-        element = data.index(i)
+    for i in range(n - 1, -1, -1):
+        parent = math.floor((i - 1) / 2)
 
-        while element != 0:
-            parent = math.floor((element - 1) / 2)
-            swap(element,parent)
-
-            element = parent
+        while parent >= 0:
+            swap_i = swap(i, parent)
+            if swap_i is not None:
+                swaps.append(swap_i)
+            i = parent
+            parent = math.floor((i - 1) / 2)
 
     return swaps
 
 
 def main():
     text = input()
-    number = int
+    number = 0
     if text[0] == "I":
         print("Enter number of nodes: ")
         number = int(input())
@@ -35,8 +38,8 @@ def main():
             print("Invalid file name")
             return
         else:
-            if "test/" not in fileName:
-                fileName = "test/" + fileName
+            if "/test/" not in fileName:
+                fileName = "/test/" + fileName
             file = open(fileName, "r")
             number = int(file.readline())
             data = list(map(int, file.readline().split()))
@@ -46,9 +49,10 @@ def main():
         return
 
     assert len(data) == number
-    swaps = build_heap(data)
+    swaps = build_heap(number ,data)
 
     # output all swaps
+
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
